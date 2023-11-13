024d0ddf3dfcd0ed6ee49dba54ab8146abf7db4d
김민규 <wls8146com@gmail.com>
2023-11-13 20:50:07 +0900

    feat: coalesce, free

--- mm.c
+++ mm.c
@@ -98,7 +98,40 @@ void *mm_malloc(size_t size) {
 /*
  * mm_free - Freeing a block does nothing.
  */
-void mm_free(void *ptr) {}
+void mm_free(void *ptr) {
+    size_t size = GET_SIZE(HDRP(ptr));
+
+    PUT(HDRP(ptr), PACK(size, FREE_BLK));
+    PUT(FTRP(ptr), PACK(size, FREE_BLK));
+    coalesce(ptr);
+}
+
+static void *coalesce(void *ptr){
+    size_t prev_alloc  = GET_ALLOC(FTRP(PREV_BLKP(ptr)));
+    size_t next_alloc  = GET_ALLOC(HDRP(NEXT_BLKP(ptr)));
+    size_t size = GET_SIZE(HDRP(ptr));
+
+    if(prev_alloc && next_alloc){
+        return ptr;
+    }
+else if(prev_alloc && !next_alloc){
+    size += GET_SIZE(HDRP(NEXT_BLKP(ptr)));
+    PUT(HDRP(ptr), PACK(size, FREE_BLK));
+    PUT(FTRP(ptr), PACK(size, FREE_BLK));
+}else if(!prev_alloc && next_alloc){
+size += GET_SIZE(HDRP(PREV_BLKP(ptr)));
+PUT(FTRP(ptr), PACK(size, FREE_BLK));
+PUT(HDRP(PREV_BLKP(ptr)), PACK(size, FREE_BLK));
+ptr = PREV_BLKP(ptr);
+}else{
+size += GET_SIZE(HDRP(PREV_BLKP(ptr))) + GET_SIZE(FTRP(NEXT_BLKP(ptr)));
+PUT(HDRP(PREV_BLKP(ptr)), PACK(size, FREE_BLK));
+    PUT(FTRP(NEXT_BLKP(ptr)), PACK(size, FREE_BLK));
+ptr = PREV_BLKP(ptr);
+}
+return ptr;
+}
+
 
 /*
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
